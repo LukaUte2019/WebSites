c45c3a1422ee6f47f5524b9512ef0e77fcaa720c
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-01-20 18:40:21 +0100

    Node JS Test

---
+++ nodejs.js
@@ -0,0 +1,12 @@
+// server.mjs
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n This is a simple http server created using Node.js');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
