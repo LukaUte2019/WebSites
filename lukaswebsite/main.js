37803c199c490203e861e7a51ce0860b30d7bb6c
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-01-28 19:39:00 +0100

    Added Light and Dark Mode function

--- lukaswebsite/main.js
+++ lukaswebsite/main.js
@@ -768,7 +768,7 @@ function calculation(input) {
 function darkMode() {
   document.body.style.backgroundColor = "black";
   document.body.style.color = "white";
-  return "Dark mode enabled";
+  return "toastnotif Dark mode enabled";
 }
 
 
@@ -781,5 +781,5 @@ function darkMode() {
 function lightMode() {
   document.body.style.backgroundColor = "white";
   document.body.style.color = "black";
-  return "Light mode enabled";
+  return "toastnotif Light mode enabled";
 }
