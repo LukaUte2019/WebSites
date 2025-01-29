78d83af47814378d853713fda15bb7a865d64ef7
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-01-29 18:19:46 +0100

    Grades app darkmode api

--- lukaswebsite/gradesapp/darkanlightmodeapi.js
+++ lukaswebsite/gradesapp/darkanlightmodeapi.js
@@ -1,5 +1,6 @@
 // This file contains the functions that are used to toggle the webpage to dark mode or light mode, or to turn on and off dark mode.
 // Add it in the html file as darkmodeapi.js. example: <script src="darkmodeapi.js"></script>
+// To be able to use the darkmode toggler, add this to CSS: body {background-color: white;color: black;} .dark-mode {background - color: black;color: white;}
 
 
 /**
@@ -56,6 +57,18 @@ function darkMode() {
     console.log("toastnotif Light mode enabled");
   }
 
+/**
+ * Toggles the webpage between dark mode and light mode.
+ *
+ * This function toggles the 'dark-mode' class on the body
+ * element, which changes the background color of the body to
+ * black and the text color of the body to white if the class is
+ * added, or changes the background color of the body to white and
+ * the text color of the body to black if the class is removed.
+ *
+ * A message is logged to the console to indicate that the dark
+ * mode has been toggled.
+ */
  function toggledarkmode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
