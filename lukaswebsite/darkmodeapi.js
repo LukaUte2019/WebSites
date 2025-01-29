22db5af5f1108e77b08d3bd3ad3bcb1770df2df4
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-01-29 17:56:53 +0100

    Added dark mode api seperate from main code

---
+++ lukaswebsite/darkmodeapi.js
@@ -0,0 +1,60 @@
+/**
+ * Toggles the webpage to dark mode styling.
+ *
+ * This function changes the background color of the body to black
+ * and changes the text color of the body to white.
+ */
+
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    console.log("toastnotif Dark mode enabled");
+    return "Dark mode enabled";
+  }
+  
+  
+  /**
+   * Toggles the webpage to light mode styling.
+   *
+   * This function changes the background color of the body to white
+   * and changes the text color of the body to black.
+   */
+  function lightMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    console.log("toastnotif Light mode enabled");
+    return "Light mode enabled";
+  }
+  
+  /**
+   * Toggles the webpage to dark mode styling.
+   *
+   * This function changes the background color of the body to black
+   * and changes the text color of the body to white.
+   */
+  
+  function darkModenoreturn() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    console.log("toastnotif Dark mode enabled");
+  }
+  
+  
+  /**
+   * Toggles the webpage to light mode styling.
+   *
+   * This function changes the background color of the body to white
+   * and changes the text color of the body to black.
+   */
+  function lightModenoreturn() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    console.log("toastnotif Light mode enabled");
+  }
+
+  
+  function toggledarkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    console.log("toastnotif Dark mode enabled or disabled");
+ }
