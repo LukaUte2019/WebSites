59311170e50e21c03ecd0d823573e7b99660b90e
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-01-29 19:10:07 +0100

    Grades app darkmode api

--- lukaswebsite/gradesapp/darkanlightmodeapi.js
+++ lukaswebsite/gradesapp/darkanlightmodeapi.js
@@ -72,5 +72,5 @@ function darkMode() {
  function toggledarkmode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-  console.log("toastnotif Dark mode enabled or disabled");
+  console.log("toastnotif Dark mode toggled");
 }
