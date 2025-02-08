c7c3cbb936b8cc3d304dedcb2a472f05b0014686
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-02-08 02:32:37 +0100

    LukaTube web app

---
+++ Youtube web app/main.js
@@ -0,0 +1,48 @@
+let videos = [];
+
+
+function loadvideos() {
+      console.log("Loading URL: http://lukaserver.ddns.net:81/videos");
+     $.ajax({
+      
+      url: "http://lukaserver.ddns.net:81/videos",
+      /**
+       * Function to set the User-Agent header before making the AJAX call.
+       * @param {Object} req - The request object to set the header on.
+       */
+
+      /**
+       * Function to handle the success of the AJAX call.
+       * This function is given the result of the AJAX call as a parameter.
+       * It logs the name of each user in the console and writes it to the page.
+       * @param {Object[]} result - The result of the AJAX call, an array of Objects.
+       */
+
+      success: function (result) {
+        for (let user of result) {
+            videos.push(`<p>${user.videoname}<p>`);
+            videos.push(`<video width="320" height="240" controls="" src=${user.videourl}></video>`);
+            document.getElementById('video-container').innerHTML = videos
+
+            // videocontainer.push(`<video width="320" height="240" controls="" src=${user.videourl}></video>`);
+            // document.getElementById("videocontainer").innerHTML = videocontainer
+            // document.write(`<h1>${user.videoname}</h1>`);
+            // document.write(`<video width="320" height="240" controls="" src=${user.videourl}></video>`);
+        }
+      },
+
+/**
+ * Function to handle errors from the AJAX call.
+ * Logs the error object to the console.
+ * @param {Object} error - The error object from the AJAX call.
+ */
+
+      error: function (error) {
+       console.log(error);
+      }
+     }
+     )
+   
+    }
+
+    loadvideos();
